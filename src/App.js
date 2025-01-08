1b1992886c7cfd9fd355ee00cda5153dc8cd3169
Sethudi Mapunya <stmapunya@gmail.com>
2025-01-08 20:26:45 +0200

    create board

--- src/App.js
+++ src/App.js
@@ -1,15 +1,65 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import useWindowSize from './useWindowSize';
 
 function App() {
   const { width, height } = useWindowSize();
-    
-  return (
-      <div>
-          <h1>Window Size</h1>
-          <p>Width: {width}px</p>
-          <p>Height: {height}px</p>
-      </div>
+  const canvasRef = useRef(null);
+  const CELLSIZE = 50;
+  const leftRightMargin = ((width % CELLSIZE) + CELLSIZE)/2;
+	const topBottomMargin = ((height % CELLSIZE) + CELLSIZE)/2;
+
+  const drawLine = (ctx, startX, startY, endX, endY) => {
+    // Draw a line
+    ctx.beginPath();
+    ctx.moveTo(startX, startY); // Start point
+    ctx.lineTo(endX, endY);    // End point
+    ctx.strokeStyle = 'rgb(134, 154, 189)';
+    ctx.lineWidth = 1;
+    ctx.stroke();
+  }
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle ='#282c34';
+    ctx.fillRect(0, 0, width, height);
+    // Clear previous drawings
+    // ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Listen for window resize events
+    // Draw a line when window is resized to maintain aspect ratio and centered on the canvas
+    // This line is a simple representation of the window edges and does not represent the actual game logic or state
+    const handleResize = () => {
+   
+      for(let x = leftRightMargin; x <= width - leftRightMargin; x += CELLSIZE) {
+        drawLine(ctx, x, topBottomMargin, x, height - topBottomMargin);
+      }
+      
+      for(let y = topBottomMargin; y <= width - topBottomMargin; y += CELLSIZE) {
+        drawLine(ctx, leftRightMargin, y, width - leftRightMargin, y);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Draw a line on app mount
+    handleResize();
+
+    // Cleanup function
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+
+  return(
+      <canvas 
+        ref={canvasRef} 
+        width={width} 
+        height={height}
+      />
+      
+
   );
 }
 export default App;
