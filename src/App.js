90e7eabcd9fdcd125f823d35d7666b8688dcef51
Sethudi Mapunya <stmapunya@gmail.com>
2025-01-09 14:03:40 +0200

    Refine File Appjs

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import World from './model/world';
 
 function App() {
   const { width, height } = useWindowSize();
-  const canvasRef = useRef(null);
   const CELLSIZE = 50;
+  let world = new World(Math.floor(width / CELLSIZE), Math.floor(height / CELLSIZE));
+  const canvasRef = useRef(null);
   const leftRightMargin = ((width % CELLSIZE) + CELLSIZE)/2;
 	const topBottomMargin = ((height % CELLSIZE) + CELLSIZE)/2;
   
@@ -23,8 +24,10 @@ function App() {
   const handleClick = (event) => {
     const col = Math.floor((event.clientX-leftRightMargin)/ CELLSIZE);
     const row= Math.floor((event.clientY-topBottomMargin) / CELLSIZE);
-    console.log(row, col);
+
+    world.setCell(row, col, true);
     fillCell((col*CELLSIZE)+ leftRightMargin, (row*CELLSIZE) + topBottomMargin);
+    console.log(world.getCell(row, col));
   }
 
   const fillCell = (startX, startY) => {
@@ -32,43 +35,25 @@ function App() {
     const ctx = canvas.getContext('2d');
     ctx.fillStyle = 'green';
     ctx.fillRect(startX, startY, CELLSIZE, CELLSIZE);
-    // let world = new World(width, height);
+    
   }
 
-  // const updateGrid = () => {
-  //   // Update game logic here
-  //   // For example, toggle cell state when clicked
-  //   for (let i = 0; i < Math.floor(height / CELLSIZE); i++) {
-  //     for (let j = 0; j < Math.floor(width / CELLSIZE); j++) {
-        
-  //       fillCell((i*CELLSIZE) + leftRightMargin, (j*CELLSIZE) + topBottomMargin);
-  //     }
-  //   }
-  //   // const cell = getCell(col, row);
-  //   // cell.alive =!cell.alive;
-  //   // setGrid(grid);
-  // }
-
-  useEffect(() => {
+  const handleResize = () => {
+    console.log('Hello');
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
-    ctx.fillStyle ='#282c34';
-    ctx.fillRect(0, 0, width, height);
-    // Clear previous drawings
-    // ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Listen for window resize events
-    // Draw a line when window is resized to maintain aspect ratio and centered on the canvas
-    // This line is a simple representation of the window edges and does not represent the actual game logic or state
-    const handleResize = () => {
-      for(let x = leftRightMargin; x <= width - leftRightMargin; x += CELLSIZE) {
-        drawLine(ctx, x, topBottomMargin, x, height - topBottomMargin);
-      }
-      for(let y = topBottomMargin; y <= width - topBottomMargin; y += CELLSIZE) {
-        drawLine(ctx, leftRightMargin, y, width - leftRightMargin, y);
-      }
-    };
+    world = new World(Math.floor(width / CELLSIZE), Math.floor(height / CELLSIZE));
+    for(let x = leftRightMargin; x <= width - leftRightMargin; x += CELLSIZE) {
+      drawLine(ctx, x, topBottomMargin, x, height - topBottomMargin);
+    }
+    for(let y = topBottomMargin; y <= width - topBottomMargin; y += CELLSIZE) {
+      drawLine(ctx, leftRightMargin, y, width - leftRightMargin, y);
+    }
+  };
 
+  useEffect(() => {
     window.addEventListener('resize', handleResize);
 
     // Draw a line on app mount
--- src/model/world.js
+++ src/model/world.js
@@ -4,7 +4,15 @@ class World{
         this.width = width;
         this.height = height;
         this.cells = Array.from({length: this.height}, () => Array.from({length: this.width}, () => false));
-        console.log(this.cells);
+        console.log("World Board");
+    }
+
+    getCell(x, y) {
+        return this.cells[x][y];
+    }
+
+    setCell(x, y, status) {
+        this.cells[x][y] = status;
     }
 }
 
